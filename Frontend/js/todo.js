c6d8f8fc8456077c862f734dc667ca6716882a80
Marius (DHBW) <marius.dhbw@thingsthinking.net>
2023-01-27 20:47:21 +0100

    added js to open and close addTaskContainer

---
+++ Frontend/js/todo.js
@@ -0,0 +1,7 @@
+function openAddTaskContainer() {
+    document.getElementById("addTaskContainer").style.display = "block";
+}
+
+function closeAddTaskContainer() {
+    document.getElementById("addTaskContainer").style.display = "none";
+}
