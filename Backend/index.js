b71ec9c690b851da656552ea8569c3660ec2cc62
Marius (DHBW) <marius.dhbw@thingsthinking.net>
2023-01-28 17:16:40 +0100

    added database connection

---
+++ Backend/index.js
@@ -0,0 +1,8 @@
+import mysql from "mysql2"
+
+const connections = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise();
