f3afecf170538f551611349fd6ded2eeafc7bcda
Raboro <marius.woerfelreal@gmail.com>
2023-02-04 15:40:45 +0100

    checked radio button of selected category

--- src/public/javascript/todo.js
+++ src/public/javascript/todo.js
@@ -2,8 +2,23 @@ function addTask() {
     //TODO add Task logic
 }
 
-function openAddTaskContainer() {
+function openAddTaskContainer(id) {
     document.getElementById("addTaskContainer").style.display = "block";
+    checkSelectedCategory(id);
+}
+
+function checkSelectedCategory(id) {
+    getRadioBox(id).childNodes.item(1).checked = true;
+}
+
+function getRadioBox(id) {
+    const radioButtons = document.getElementsByClassName("chooseCategory").item(0).childNodes;
+    if (id === "todoAddButton") {
+        return radioButtons.item(1);
+    } else if (id === "inProgressAddButton") {
+        return radioButtons.item(3);
+    }
+    return radioButtons.item(5);
 }
 
 function closeAddTaskContainer() {
