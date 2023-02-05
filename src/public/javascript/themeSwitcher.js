a7366fda56e0f3b1241ddbe01445a98f6e4ebe21
Raboro <marius.woerfelreal@gmail.com>
2023-02-05 17:08:47 +0100

    extracted rule construction

--- src/public/javascript/themeSwitcher.js
+++ src/public/javascript/themeSwitcher.js
@@ -16,43 +16,66 @@ function getStylesheetWithRules() {
 }
 
 function switchToWhiteTheme(stylesheet) {
-    stylesheet.insertRule(":root {\n" +
-        "    --mainBackgroundColor: white;\n" +
-        "    --headingColor: black;\n" +
-        "    --categoryBorderColor: rgba(255, 255, 255, 0.281);\n" +
-        "    --categoryBoxShadowColor: rgba(131, 131, 131, 0.295);\n" +
-        "    --categoryBackgroundColor: rgba(206, 205, 205, 0.24);\n" +
-        "    --categoryHeadingBoxShadowColor: rgba(146, 126, 126, 0.178);\n" +
-        "    --headingTodoBackgroundColor: rgba(128, 128, 128, 0.199);\n" +
-        "    --headingTodoColor: grey;\n" +
-        "    --headingInProgressBackgroundColor: rgba(121, 177, 230, 0.329);\n" +
-        "    --headingInProgressColor: rgb(121, 177, 230);\n" +
-        "    --headingDoneBackgroundColor: rgba(126, 228, 126, 0.274);\n" +
-        "    --headingDoneColor: rgba(63, 150, 63, 0.699);\n" +
-        "    --addButtonBackgroundColor: rgba(128, 128, 128, 0.144);\n" +
-        "    --addButtonColor: black;\n" +
-        "    --addButtonHoverBackgroundColor: rgba(128, 128, 128, 0.212);\n" +
-        "    --addButtonHoverBoxShadowColor: rgba(146, 126, 126, 0.089);\n" +
-        "}");
+    const rule = constructRule({
+        mainBackground: "white",
+        heading: "black",
+        categoryBorder: "rgba(255, 255, 255, 0.281)",
+        categoryBoxShadow: "rgba(131, 131, 131, 0.295)",
+        categoryBackground: "rgba(206, 205, 205, 0.24)",
+        categoryHeadingBoxShadow: "rgba(146, 126, 126, 0.178)",
+        headingTodoBackground: "rgba(128, 128, 128, 0.199)",
+        headingTodo: "grey",
+        headingInProgressBackground: "rgba(121, 177, 230, 0.329)",
+        headingInProgress: "rgb(121, 177, 230)",
+        headingDoneBackground: "rgba(126, 228, 126, 0.274)",
+        headingDone: "rgba(63, 150, 63, 0.699)",
+        addButtonBackground: "rgba(128, 128, 128, 0.144)",
+        addButton: "black",
+        addButtonHoverBackground: "rgba(128, 128, 128, 0.212)",
+        addButtonHoverBoxShadow: "rgba(146, 126, 126, 0.089)"
+    });
+    stylesheet.insertRule(rule);
 }
 
 function switchToDarkTheme(stylesheet) {
-    stylesheet.insertRule(":root {\n" +
-        "    --mainBackgroundColor: rgba(0, 0, 0, 0.3);\n" +
-        "    --headingColor: black;\n" +
-        "    --categoryBorderColor: rgba(255, 255, 255, 0.281);\n" +
-        "    --categoryBoxShadowColor: rgba(131, 131, 131, 0.295);\n" +
-        "    --categoryBackgroundColor: rgba(206, 205, 205, 0.24);\n" +
-        "    --categoryHeadingBoxShadowColor: rgba(146, 126, 126, 0.178);\n" +
-        "    --headingTodoBackgroundColor: rgba(128, 128, 128, 0.199);\n" +
-        "    --headingTodoColor: grey;\n" +
-        "    --headingInProgressBackgroundColor: rgba(121, 177, 230, 0.329);\n" +
-        "    --headingInProgressColor: rgb(121, 177, 230);\n" +
-        "    --headingDoneBackgroundColor: rgba(126, 228, 126, 0.274);\n" +
-        "    --headingDoneColor: rgba(63, 150, 63, 0.699);\n" +
-        "    --addButtonBackgroundColor: rgba(128, 128, 128, 0.144);\n" +
-        "    --addButtonColor: black;\n" +
-        "    --addButtonHoverBackgroundColor: rgba(128, 128, 128, 0.212);\n" +
-        "    --addButtonHoverBoxShadowColor: rgba(146, 126, 126, 0.089);\n" +
-        "}");
+    const rule = constructRule({
+        mainBackground: "rgba(0, 0, 0, 0.3)",
+        heading: "black",
+        categoryBorder: "rgba(255, 255, 255, 0.281)",
+        categoryBoxShadow: "rgba(131, 131, 131, 0.295)",
+        categoryBackground: "rgba(206, 205, 205, 0.24)",
+        categoryHeadingBoxShadow: "rgba(146, 126, 126, 0.178)",
+        headingTodoBackground: "rgba(128, 128, 128, 0.199)",
+        headingTodo: "grey",
+        headingInProgressBackground: "rgba(121, 177, 230, 0.329)",
+        headingInProgress: "rgb(121, 177, 230)",
+        headingDoneBackground: "rgba(126, 228, 126, 0.274)",
+        headingDone: "rgba(63, 150, 63, 0.699)",
+        addButtonBackground: "rgba(128, 128, 128, 0.144)",
+        addButton: "black",
+        addButtonHoverBackground: "rgba(128, 128, 128, 0.212)",
+        addButtonHoverBoxShadow: "rgba(146, 126, 126, 0.089)"
+    });
+    stylesheet.insertRule(rule);
+}
+
+function constructRule(colors) {
+    return ":root {\n" +
+        `    --mainBackgroundColor: ${colors.mainBackground};\n` +
+        `    --headingColor: ${colors.heading};\n` +
+        `    --categoryBorderColor: ${colors.categoryBorder};\n` +
+        `    --categoryBoxShadowColor: ${colors.categoryBoxShadow};\n` +
+        `    --categoryBackgroundColor: ${colors.categoryBackground};\n` +
+        `    --categoryHeadingBoxShadowColor: ${colors.categoryHeadingBoxShadow};\n` +
+        `    --headingTodoBackgroundColor: ${colors.headingTodoBackground};\n` +
+        `    --headingTodoColor: ${colors.headingTodo};\n` +
+        `    --headingInProgressBackgroundColor: ${colors.headingInProgressBackground};\n` +
+        `    --headingInProgressColor: ${colors.headingInProgress};\n` +
+        `    --headingDoneBackgroundColor: ${colors.headingDoneBackground};\n` +
+        `    --headingDoneColor: ${colors.headingDone};\n` +
+        `    --addButtonBackgroundColor: ${colors.addButtonBackground};\n` +
+        `    --addButtonColor: ${colors.addButton};\n` +
+        `    --addButtonHoverBackgroundColor: ${colors.addButtonHoverBackground};\n` +
+        `    --addButtonHoverBoxShadowColor: ${colors.addButtonHoverBoxShadow};\n` +
+        `}`;
 }
