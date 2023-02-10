ac576bc549c67774f994425ab0b3fcd12906ed87
Raboro <raborogit@gmail.com>
2023-02-10 17:11:06 +0100

    added semicolon at the end of each line

--- src/config/database.js
+++ src/config/database.js
@@ -14,7 +14,7 @@ const connections = mysql.createPool({
 
 async function getAllTasksByCategory(id) {
     const data = await connections.query("SELECT * FROM tasks WHERE category = ? ORDER BY position ASC", id);
-    return data[0]
+    return data[0];
 }
 
 async function addTask(idCategory, title, content, dueDate, user, position) {
@@ -23,38 +23,38 @@ async function addTask(idCategory, title, content, dueDate, user, position) {
 
 async function deleteTask(id) {
     const data = await connections.query("DELETE FROM tasks WHERE tasks.id = ?", id);
-    return data[0]
+    return data[0];
 }
 
 async function changePositionOfTask(id, position) {
-    const data = await connections.query("UPDATE tasks SET position = ? WHERE tasks.id = ?", [position, id])
-    return data[0]
+    const data = await connections.query("UPDATE tasks SET position = ? WHERE tasks.id = ?", [position, id]);
+    return data[0];
 }
 
 async function changeCategoryAndPosition(id, position, category) {
     const data = await connections.query("UPDATE tasks SET category = ?, position = ? WHERE task.id = ?", [category, position, id]);
-    return data[0]
+    return data[0];
 }
 
 async function updateTask(id, idCategory, title, content, dueDate, position) {
     const data = await connections.query("UPDATE tasks SET category = ?, title = ?, content = ?, dueDate = ?, position = ? WHERE id = ?", [idCategory, title, content, dueDate, position, id]);
-    return data[0]
+    return data[0];
 }
 
 
 async function signIn(email, pwd) {
     const data = connections.query("SELECT email FROM users WHERE email = ? AND pwd = ?", [email, pwd]);
-    return data[0]
+    return data[0];
 }
 
 async function signUpCheckUniqueEmail(email) {
     const data = connections.query("SELECT email FROM users WHERE email = ?", email);
-    return data[0]
+    return data[0];
 }
 
 async function signUpInsertNewUser(firstName, lastName, email, pwd) {
     const data = connections.query("INSERT INTO users VALUES(?,?,?,?)", [firstName, lastName, email, pwd]);
-    return data[0]
+    return data[0];
 }
 
 console.log(await getAllTasksByCategory(3));
