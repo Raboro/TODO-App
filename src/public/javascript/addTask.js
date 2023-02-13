5d31395d7b7e389496ce1b8d28bbe1b930827e7c
janikpiehler <janik.piehler@gmx.de>
2023-02-13 11:25:44 +0100

    add drag to addTask

--- src/public/javascript/addTask.js
+++ src/public/javascript/addTask.js
@@ -1,14 +1,27 @@
-function move(){
-    addTaskContainer.addEventListener("mousemove", track);
-}
+let addTsk = document.getElementById("addTaskContainer");
+let moveElement = false;
+let initialX = 0;
+let initialY = 0;
 
-function track(e){
-    var x = e.clientX;
-    var y = e.clientY;
-    addTaskContainer.style.top =y+"px";
-    addTaskContainer.style.left =x+"px";
-}
+addTsk.addEventListener("mousedown", (e) =>{
+    initialX = e.clientX;
+    initialY = e.clientY;
+    moveElement = true;
+});
 
-function stop(){
-    addTaskContainer.removeEventListener("mousemove", track)
-}
+addTsk.addEventListener("mousemove", (e) => {
+    if(moveElement == true){
+    e.preventDefault();
+    let newX = e.clientX;
+    let newY = e.clientY;
+    addTsk.style.top = addTsk.offsetTop - (initialY - newY)+"px";
+    addTsk.style.left = addTsk.offsetLeft - (initialX - newX)+"px";
+
+    initialX = newX;
+    initialY = newY;
+    }
+});
+
+addTsk.addEventListener("mouseup", (e) =>{
+    moveElement = false;
+});
