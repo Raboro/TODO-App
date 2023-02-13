b06618896dcbb09635e68a6a9b297c9eae130d1d
Raboro <raborogit@gmail.com>
2023-02-13 12:52:24 +0100

    extracted database access in service

---
+++ src/services/database.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import path from "path";
+import mysql from "mysql2";
+
+dotenv.config({path: path.join(process.cwd().replace("src\\services", ".env"))});
+
+function getConnection() {
+    return  mysql.createPool({
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
+    }).promise();
+}
+
+export async function executeQuery(query) {
+    const connection = getConnection();
+    const data = await connection.query(query);
+    await connection.end();
+    return data[0];
+}
