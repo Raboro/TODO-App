786bf4d51486b45333e616fc5e22ecf8d0965ca5
Raboro <raborogit@gmail.com>
2023-02-16 12:13:26 +0100

    used user controller

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,12 +1,13 @@
 import express from 'express';
+import {getUser} from "../controllers/user.js";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-
+    return getUser(req);
 })
 
 router.post("/", (req, res) => {
-
+    return addUser(req);
 })
 
 export default router;
