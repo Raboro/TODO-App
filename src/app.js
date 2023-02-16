e507668fc5ea67f51af940a9fc96e8ebf62844d4
Raboro <raborogit@gmail.com>
2023-02-16 11:38:45 +0100

    used user routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import printEndpointCall from './middleware/endpointCall.js';
+import userRouter from './routes/user.js';
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use('/api/user', userRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(printEndpointCall);
