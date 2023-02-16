adaf38fcd162003c2a5fc857942f9b53e2e11813
janikpiehler <janik.piehler@gmx.de>
2023-02-16 13:36:30 +0100

    add Sidebar

---
+++ src/public/javascript/sidebar.js
@@ -0,0 +1,9 @@
+function openSidebar(){
+    document.getElementById("sidebar").style.width = "75px";
+    document.getElementById("openBar").hidden = true;
+}
+
+function closeSidebar(){
+    document.getElementById("sidebar").style.width = "0px";
+    document.getElementById("openBar").hidden = false;
+}
