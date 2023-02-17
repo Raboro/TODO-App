882fa89862151d8ebf2285e8c4c0e2e73349d368
Raboro <raborogit@gmail.com>
2023-02-17 21:44:56 +0100

    merged new changes in app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
 import path from 'path';
-import printEndpointCall from './middleware/endpointCall.js';
+import {printEndpointCall} from './middleware/endpointCall.js';
+import {allowCrossOrigin} from "./middleware/crossOrigin.js";
 import userRouter from './routes/user.js';
+import signInRouter from './routes/signIn.js';
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/user', userRouter);
 app.use(printEndpointCall);
+app.use(allowCrossOrigin)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(process.cwd(), '/src/public' )));
+
+app.use('/user', userRouter);
+app.use('/signIn', signInRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
