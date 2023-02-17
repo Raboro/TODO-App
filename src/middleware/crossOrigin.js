4521261f3175ecbe94762cb96c4f1f3c3b609cef
Raboro <raborogit@gmail.com>
2023-02-17 21:40:07 +0100

    added middleware to fix cross-origin

---
+++ src/middleware/crossOrigin.js
@@ -0,0 +1,6 @@
+export const allowCrossOrigin = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
