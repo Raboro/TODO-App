a603b451c3e0ff9d9299932e4449bf407ce2d8f9
Raboro <raborogit@gmail.com>
2023-02-17 22:02:28 +0100

    made signIn connect to database

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -8,13 +8,7 @@ const users = [
 ];
 
 export async function signInUser(req, res) {
-    for (let i = 0; i < users.length; i++) {
-        if (users[i].email === req.body.email && users[i].password === req.body.password) {
-            res.send(JSON.stringify('response: yes sir'));
-            return;
-        }
-    }
-    res.send(JSON.stringify('response : no sir'));
+    res.send(JSON.stringify(await signIn(req.body.email, req.body.password)));
 }
 
 export function addUser(req, res) {
