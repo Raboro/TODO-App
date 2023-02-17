5a9bbbb623ac15ce85947d41407a1929c3cd5509
Raboro <raborogit@gmail.com>
2023-02-17 22:01:30 +0100

    wrapped db inserts into ''

--- src/services/task.js
+++ src/services/task.js
@@ -1,25 +1,25 @@
 import { executeQuery } from './database.js';
 
 async function getAllTasksByCategory(id) {
-    return await executeQuery(`SELECT * FROM tasks WHERE category = ${id} ORDER BY position ASC`);
+    return await executeQuery(`SELECT * FROM tasks WHERE category = '${id}' ORDER BY position ASC`);
 }
 
 async function addTask(category, title, content, dueDate, user, position) {
-    return await executeQuery(`INSERT INTO tasks VALUES(NULL, ${category}, ${title}, ${content}, ${dueDate}, ${user}, ${position}, NULL)`);
+    return await executeQuery(`INSERT INTO tasks VALUES('NULL', '${category}', '${title}', '${content}', '${dueDate}', '${user}', '${position}', 'NULL')`);
 }
 
 async function deleteTask(id) {
-    return await executeQuery(`DELETE FROM tasks WHERE tasks.id = ${id}`);
+    return await executeQuery(`DELETE FROM tasks WHERE tasks.id = '${id}'`);
 }
 
 async function changePositionOfTask(id, position) {
-    return await executeQuery(`UPDATE tasks SET position = ${position} WHERE tasks.id = ${id}`);
+    return await executeQuery(`UPDATE tasks SET position = '${position}' WHERE tasks.id = '${id}'`);
 }
 
 async function changeCategoryAndPosition(id, position, category) {
-    return await executeQuery(`UPDATE tasks SET category = ${category}, position = ${position} WHERE task.id = ${id}`);
+    return await executeQuery(`UPDATE tasks SET category = '${category}', position = '${position}' WHERE task.id = '${id}'`);
 }
 
 async function updateTask(id, category, title, content, dueDate, position) {
-    return await executeQuery(`UPDATE tasks SET category = ${category}, title = ${title}, content = ${content}, dueDate = ${dueDate}, position = ${position} WHERE id = ${id}`);
+    return await executeQuery(`UPDATE tasks SET category = '${category}', title = '${title}', content = '${content}', dueDate = '${dueDate}', position = '${position}' WHERE id = '${id}'`);
 }
--- src/services/user.js
+++ src/services/user.js
@@ -1,13 +1,13 @@
 import { executeQuery } from './database.js';
 
 export async function signIn(email, password) {
-    return executeQuery(`SELECT email FROM users WHERE email = ${email} AND pwd = ${password}`);
+    return executeQuery(`SELECT email FROM users WHERE email = '${email}' AND pwd = '${password}'`);
 }
 
 export async function signUpCheckUniqueEmail(email) {
-    return executeQuery(`SELECT email FROM users WHERE email = ${email}`);
+    return executeQuery(`SELECT email FROM users WHERE email = '${email}'`);
 }
 
 export async function signUpInsertNewUser(firstName, lastName, email, password) {
-    return executeQuery(`INSERT INTO users VALUES(${firstName}, ${lastName}, ${email}, ${password})`);
+    return executeQuery(`INSERT INTO users VALUES('${firstName}', '${lastName}', '${email}', '${password}')`);
 }
