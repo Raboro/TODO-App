1da8bdb449036ad420ae12a2f271f668bc1588a2
Raboro <raborogit@gmail.com>
2023-02-17 21:43:11 +0100

    replaced unnecessary return with await

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,16 +2,16 @@ import express from 'express';
 import {addUser, signInUser, logoutUser} from "../controllers/user.js";
 const router = express.Router();
 
-router.post("/signin", (req, res) => {
-    return signInUser(req, res);
+router.post("/signin", async (req, res) => {
+    await signInUser(req, res);
 })
 
-router.post("/signup", (req, res) => {
-    return addUser(req, res);
+router.post("/signup", async (req, res) => {
+    await addUser(req, res);
 })
 
-router.get("/logout", (req, res) => {
-    return logoutUser(req, res);
+router.get("/logout", async (req, res) => {
+    await logoutUser(req, res);
 })
 
 export default router;
