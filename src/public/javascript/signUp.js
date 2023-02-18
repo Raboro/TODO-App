efa245ade9100adc384bcbf0511cd8eb2676f2b7
Raboro <raborogit@gmail.com>
2023-02-18 16:47:09 +0100

    added signUp in frontend

--- src/public/javascript/signUp.js
+++ src/public/javascript/signUp.js
@@ -7,4 +7,24 @@ const cSendData = function sendData() {
 
 };
 
+async function signUp() {
+    const password = document.getElementById('password').value;
+    const repeatedPassword =document.getElementById('passwordRepeated').value ;
+    if (password !== repeatedPassword) {
+        return;
+    }
+    const rawData = await fetch('http://localhost:8080/user/signUp', {
+        method: 'POST',
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            firstName: document.getElementById('firstName').value,
+            lastName: document.getElementById('lastName').value,
+            email: document.getElementById('email').value,
+            password: password
+        })
+    });
+    console.log(await rawData.json());
+}
+
 document.getElementById('buttonSignUp').addEventListener('click', cSendData);
