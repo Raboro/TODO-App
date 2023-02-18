03195771f8def8950f571d84f4c4c5a9e0697100
Raboro <raborogit@gmail.com>
2023-02-18 17:43:23 +0100

    added formData validation

--- src/public/javascript/signUp.js
+++ src/public/javascript/signUp.js
@@ -8,23 +8,48 @@ const cSendData = function sendData() {
 };
 
 async function signUp() {
-    const password = document.getElementById('password').value;
-    const repeatedPassword =document.getElementById('passwordRepeated').value ;
-    if (password !== repeatedPassword) {
-        return;
+    const formData = fetchForm();
+    if (isFormValid(formData)) {
+        delete formData.passwordRepeated
+        await addUser(formData);
     }
+}
+
+function fetchForm() {
+    return {
+        firstName: document.getElementById('firstName').value,
+        lastName: document.getElementById('lastName').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        passwordRepeated: document.getElementById('passwordRepeated').value,
+    };
+}
+
+function isFormValid(formData) {
+    return areValuesValid(formData) && arePasswordsEqual(formData);
+}
+
+const areValuesValid = (formData) => {
+    for (let key in formData) {
+        if (isInvalidValue(formData[key]))
+            return false;
+    }
+    return true;
+};
+
+const isInvalidValue = (value) => value === '';
+
+const arePasswordsEqual = (formData) => formData.password === formData.passwordRepeated;
+
+async function addUser(userData) {
     const rawData = await fetch('http://localhost:8080/user/signUp', {
         method: 'POST',
         mode: 'cors',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            firstName: document.getElementById('firstName').value,
-            lastName: document.getElementById('lastName').value,
-            email: document.getElementById('email').value,
-            password: password
-        })
+        body: JSON.stringify(userData)
     });
     console.log(await rawData.json());
+
 }
 
 document.getElementById('buttonSignUp').addEventListener('click', cSendData);
