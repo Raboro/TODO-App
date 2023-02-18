b6cab6b9e3445d4f19ae8a033cc2f56c003c1c7a
Raboro <raborogit@gmail.com>
2023-02-18 16:46:44 +0100

    removed local storage and edited addUser

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,26 +1,15 @@
 import { signIn, signUpCheckUniqueEmail, signUpInsertNewUser } from '../services/user.js';
 
-const users = [
-    {
-        email: 'test@test.com',
-        password: 'test'
-    }
-];
 
 export async function signInUser(req, res) {
     res.send(JSON.stringify(await signIn(req.body.email, req.body.password)));
 }
 
-export function addUser(req, res) {
-    const user = {
-        firstName: req.headers.firstname,
-        lastName: req.headers.lastname,
-        email: req.headers.email,
-        password: req.headers.password
-    };
-    users.push(user);
-    console.log(users);
-    res.send('ok');
+export async function addUser(req, res) {
+    const data = await signUpCheckUniqueEmail(req.body.email);
+    if (data.length === 0) {
+        console.log(await signUpInsertNewUser(req.body.firstName, req.body.lastName, req.body.email, req.body.password))
+    }
 }
 
 export async function logoutUser(req, res) {
