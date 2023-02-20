705cccc33619b40dccbaf5d5090c22ac22b326f8
Raboro <raborogit@gmail.com>
2023-02-20 14:08:12 +0100

    fixed 7 errors

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import { allowCrossOrigin } from './middleware/crossOrigin.js';
 import userRouter from './routes/user.js';
 import signInRouter from './routes/signIn.js';
 import signUpRouter from './routes/signUp.js';
-import chalk from "chalk";
+import chalk from 'chalk';
 
 dotenv.config({ path: path.join(process.cwd(), '.env') });
 
@@ -25,7 +25,6 @@ app.use('/signIn', signInRouter);
 app.use('/signUp', signUpRouter);
 
 app.listen(PORT, (err) => {
-    if (err)
-        console.log(`There is a error: ${err}`);
-    console.log(chalk.green(`[SERVER]`) + ` running on port:` + chalk.green(` ${PORT}`));
+    if (err) { console.log(`There is a error: ${err}`); }
+    console.log(chalk.green('[SERVER]') + ' running on port:' + chalk.green(` ${PORT}`));
 });
--- src/middleware/endpointCall.js
+++ src/middleware/endpointCall.js
@@ -1,6 +1,6 @@
-import chalk from "chalk";
+import chalk from 'chalk';
 
 export const printEndpointCall = (req, res, next) => {
-    console.log(chalk.green(`[SERVER]`) +  ` ${req.method} was called by` + chalk.green(` ${req.path}`));
+    console.log(chalk.green('[SERVER]') + ` ${req.method} was called by` + chalk.green(` ${req.path}`));
     next();
 };
