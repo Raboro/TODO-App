dba5fbf409d7b7ff97d7917625f2fda409a903fa
Raboro <raborogit@gmail.com>
2023-02-21 14:22:48 +0100

    exported funcitons

--- src/services/task.js
+++ src/services/task.js
@@ -1,25 +1,25 @@
 import { executeQuery } from './database.js';
 
-async function getAllTasksByCategory(idCategory, user) {
+export async function getAllTasksByCategory(idCategory, user) {
     return await executeQuery(`SELECT * FROM tasks WHERE category = '${idCategory}' AND user = '${user}' ORDER BY position ASC`);
 }
 
-async function addTask(category, title, content, dueDate, user, position) {
+export async function addTask(category, title, content, dueDate, user, position) {
     return await executeQuery(`INSERT INTO tasks VALUES('NULL', '${category}', '${title}', '${content}', '${dueDate}', '${user}', '${position}', 'NULL')`);
 }
 
-async function deleteTask(id) {
+export async function deleteTask(id) {
     return await executeQuery(`DELETE FROM tasks WHERE tasks.id = '${id}'`);
 }
 
-async function changePositionOfTask(id, position) {
+export async function changePositionOfTask(id, position) {
     return await executeQuery(`UPDATE tasks SET position = '${position}' WHERE tasks.id = '${id}'`);
 }
 
-async function changeCategoryAndPosition(id, position, category) {
+export async function changeCategoryAndPosition(id, position, category) {
     return await executeQuery(`UPDATE tasks SET category = '${category}', position = '${position}' WHERE task.id = '${id}'`);
 }
 
-async function updateTask(id, category, title, content, dueDate, position) {
+export async function updateTask(id, category, title, content, dueDate, position) {
     return await executeQuery(`UPDATE tasks SET category = '${category}', title = '${title}', content = '${content}', dueDate = '${dueDate}', position = '${position}' WHERE id = '${id}'`);
 }
