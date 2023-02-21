363ba36e0733d4424e73c24af676713b8cdc1ddb
Raboro <raborogit@gmail.com>
2023-02-21 14:33:10 +0100

    inserted authentication middleware to task routes

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,28 +1,29 @@
 import express from 'express';
 import { loadAllTasks, addNewTask, deleteTask, changePositionOfTask, changeCategoryAndPositionOfTask, updateTask } from '../controllers/task.js';
+import {authenticateToken} from "../middleware/jwtAuthentification.js";
 const router = express.Router();
 
-router.post('/loadAllTasks', async(req, res) => {
+router.get('/loadAllTasks', authenticateToken, async(req, res) => {
     await loadAllTasks(req, res);
 });
 
-router.post('/addTask', async(req, res) => {
+router.post('/addTask', authenticateToken, async(req, res) => {
     await addNewTask(req, res);
 });
 
-router.post('/deleteTask', async(req, res) => {
+router.post('/deleteTask', authenticateToken, async(req, res) => {
     await deleteTask(req, res);
 });
 
-router.post('/changePositionOfTask', async(req, res) => {
+router.post('/changePositionOfTask', authenticateToken, async(req, res) => {
     await changePositionOfTask(req, res);
 });
 
-router.post('/changeCategoryAndPositionOfTask', async(req, res) => {
+router.post('/changeCategoryAndPositionOfTask', authenticateToken, async(req, res) => {
     await changeCategoryAndPositionOfTask(req, res);
 });
 
-router.post('/updateTask', async(req, res) => {
+router.post('/updateTask', authenticateToken, async(req, res) => {
     await updateTask(req, res);
 });
 
