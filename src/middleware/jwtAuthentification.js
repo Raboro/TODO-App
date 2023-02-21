c9905f3aa527003d904b97cccc4d7ffa367c4c0a
Raboro <raborogit@gmail.com>
2023-02-21 14:16:55 +0100

    added jwt token authentication middleware

---
+++ src/middleware/jwtAuthentification.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(process.cwd().replace('src\\middleware', '.env')) });
+
+export const authenticateToken = (req, res, next) => {
+    const token = req.cookies.token;
+    try {
+        req.email = jwt.verify(token, process.env.JWT_SECRET);
+        next();
+    } catch (err) {
+        res.clearCookie('token');
+        return res.redirect('/signIn');
+    }
+}
