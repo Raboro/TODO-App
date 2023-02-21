79353e10a2696bfc75ed0d2e227590ed557d0b5b
Raboro <raborogit@gmail.com>
2023-02-21 13:53:49 +0100

    added JWT token when user login

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,12 +1,28 @@
 import { signIn, signUpCheckUniqueEmail, signUpInsertNewUser } from '../services/user.js';
+import dotenv from 'dotenv';
+import path from 'path';
+import jwt from 'jsonwebtoken';
+import chalk from 'chalk';
+
+dotenv.config({ path: path.join(process.cwd().replace('src\\controllers', '.env')) });
 
 export async function signInUser(req, res) {
-    res.send(JSON.stringify(await signIn(req.body.email, req.body.password)));
+    const email = await signIn(req.body.email, req.body.password);
+    if (email[0] !== undefined) {
+        console.log(chalk.green('[SERVER]') + ' user:' + chalk.green(` ${req.body.email}`), 'is logging in');
+        const token = jwt.sign(JSON.stringify(email), process.env.JWT_SECRET);
+        res.cookie('token', token);
+        res.send();
+    } else {
+        res.send(JSON.stringify('Wrong user data'));
+    }
 }
 
 export async function addUser(req, res) {
     if (isNotAlreadyInUse(await signUpCheckUniqueEmail(req.body.email))) {
         console.log(await signUpInsertNewUser(req.body.firstName, req.body.lastName, req.body.email, req.body.password));
+    } else {
+        res.send(JSON.stringify('response: User could not be added'));
     }
 }
 
