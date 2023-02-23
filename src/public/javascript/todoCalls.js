123e74a27bdb334047c3e6cf4ed0181904cb757f
Raboro <raborogit@gmail.com>
2023-02-23 15:00:44 +0100

    fixed styles

--- src/public/javascript/todoCalls.js
+++ src/public/javascript/todoCalls.js
@@ -1,16 +1,13 @@
 
 fetch('http://localhost:8080/task/getAllTasksByCategory')
     .then(res => {
-        if(res.ok) { console.log("GET request successful")}
-        else { console.log("GET failed")}
-        return res
+        if (res.ok) { console.log('GET request successful'); } else { console.log('GET failed'); }
+        return res;
     })
     .then(res => res.json())
     .then(data => addTasksByCategory())
-    .catch(error => console.log(error))    
+    .catch(error => console.log(error));
 
-
-function addTasksByCategory(){
-    console.log(data)
-    console.log("test")
+function addTasksByCategory() {
+    console.log('test');
 }
