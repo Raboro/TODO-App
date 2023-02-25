ad1b9385cb8e7587377a6744ef5f84da8973d2b8
Raboro <raborogit@gmail.com>
2023-02-25 13:09:13 +0100

    removed unnecessary return and add await before query

--- src/services/user.js
+++ src/services/user.js
@@ -1,13 +1,13 @@
 import { executeQuery } from './database.js';
 
 export async function signIn(email, password) {
-    return executeQuery(`SELECT email FROM users WHERE email = '${email}' AND pwd = '${password}'`);
+    return await executeQuery(`SELECT email FROM users WHERE email = '${email}' AND pwd = '${password}'`);
 }
 
 export async function signUpCheckUniqueEmail(email) {
-    return executeQuery(`SELECT email FROM users WHERE email = '${email}'`);
+    return await executeQuery(`SELECT email FROM users WHERE email = '${email}'`);
 }
 
 export async function signUpInsertNewUser(firstName, lastName, email, password) {
-    return executeQuery(`INSERT INTO users VALUES('${firstName}', '${lastName}', '${email}', '${password}')`);
+    await executeQuery(`INSERT INTO users VALUES('${firstName}', '${lastName}', '${email}', '${password}')`);
 }
