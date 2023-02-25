55042c418c7189bdad6c7861018949f7bbd17db0
Raboro <raborogit@gmail.com>
2023-02-25 17:17:16 +0100

    simplified getAllTasks fetch

--- src/public/javascript/todoCalls.js
+++ src/public/javascript/todoCalls.js
@@ -1,11 +1,6 @@
 fetch('http://localhost:8080/task/getAllTasks')
-    .then(res => {
-        if (res.ok) { console.log('GET request successful'); } else { console.log('GET failed'); }
-        return res;
-    })
     .then(res => res.json())
     .then(data => addTasksByCategory(data))
-    .catch(error => console.log(error));
 
 function addTasksByCategory(data) {
     addTasks(data.todo);
