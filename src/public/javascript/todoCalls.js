ab17fbcfd937f2c9110f043ea88dd2dd54393bc2
janikpiehler <janik.piehler@gmx.de>
2023-02-28 13:45:55 +0100

    structured frontend

--- src/public/javascript/todoCalls.js
+++ src/public/javascript/todoCalls.js
@@ -44,3 +44,13 @@ function logout() { // eslint-disable-line no-unused-vars
     fetch('http://localhost:8080/user/logout');
     window.location.href = 'http://localhost:8080/signIn';
 }
+
+// eslint-disable-next-line no-unused-vars
+async function callApi(call) {
+    return await fetch(call.url, {
+        method: call.method,
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+        body: call.body
+    });
+}
